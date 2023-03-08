318bfd1b9b22a49e8b1e63c1fceb71b945006619
VARUN <varunaltekar2@gmail.com>
2023-03-08 13:59:34 +0530

    update book feature

--- src/main/java/com/http/demo/controller/BookController.java
+++ src/main/java/com/http/demo/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ import com.http.demo.repo.BookRepository;
 @RestController
 @RequestMapping(path="/api/books")
 public class BookController {
-	
+
 	/**
 	 * Logback is provided out of the box with Spring Boot when you use one of the 
 	 * Spring Boot starter dependencies
@@ -38,7 +39,7 @@ public class BookController {
 
 	@PostMapping
 	public ResponseEntity<?> createBook(@Valid @RequestBody Book book, UriComponentsBuilder ucBuilder) {
-		
+
 		/**
 		 * 1. Creating a book needs first to check a book is not already present in the database 
 		 * with the same ISBN
@@ -51,7 +52,7 @@ public class BookController {
 		}
 		LOGGER.info("saving book");
 		bookRepository.save(book);
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		// uri templates & UCB to specify resource location
 		/**
@@ -60,20 +61,20 @@ public class BookController {
 		 */
 		headers.setLocation(ucBuilder.path("/api/books/{isbn}").buildAndExpand(book.getIsbn()).toUri());
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		
+
 		LOGGER.info("setting headers and sending response");
-		
+
 		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 	}
-	
+
 	@GetMapping("/{isbn}")
 	public ResponseEntity<Book> getBook(@PathVariable(name="isbn") String isbn){
 		return bookRepository.findByIsbn(isbn).
-		map(book -> new ResponseEntity<>(book, HttpStatus.OK)).
-		orElseThrow(() -> new BookNotFoundException(isbn));
-		
+				map(book -> new ResponseEntity<>(book, HttpStatus.OK)).
+				orElseThrow(() -> new BookNotFoundException(isbn));
+
 	}
-	
+
 	@PutMapping("/{isbn}")
 	public ResponseEntity<Book> updateBook(@PathVariable(name="isbn") String isbn, @Valid @RequestBody Book book){
 		return bookRepository.findByIsbn(isbn)
@@ -82,17 +83,31 @@ public class BookController {
 					bookUpdated.setDescription(book.getDescription());
 					bookUpdated.setPublisher(book.getPublisher());
 					bookUpdated.setTitle(book.getTitle());
-					
+
 					bookUpdated.addAuthor(book.getAuthors().iterator().next());
-					
+
 					bookRepository.save(bookUpdated);
-					
+
 					return new ResponseEntity<Book>(bookUpdated, HttpStatus.OK);
 				}).orElseThrow(() -> new BookNotFoundException(isbn));
 	}
-	
-	
-	
-	
-	
+
+	/**
+	 * 1. check if book exits for isbn, then delete
+	 * 		else throw exception
+	 * @param isbn
+	 * @return
+	 */
+	@DeleteMapping("/{isbn}")
+	public ResponseEntity<?> deleteBook(@PathVariable(name = "isbn") String isbn){
+		return bookRepository.findByIsbn(isbn).
+			map(bookDelete -> {
+				bookRepository.delete(bookDelete);
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			})
+			.orElseThrow(() -> new BookNotFoundException("Wrong isbn"));
+	}
+
+
+
 }
